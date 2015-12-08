15d230c7b74ab399ce82ae711ea3aa085a4103b1
jccultima123 <johncyrillcorsanes@gmail.com>
2015-12-09 00:00:34 +0800

    [EXTRA] Added Posts Controller with some demos for tests

---
+++ application/views/crush/error.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>ERROR!</title>
+</head>
+<body>
+<h2>Boy or Girl Only!</h2>
+</body>
+</html>
---
+++ application/views/crush/girl.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Crush - Girl</title>
+</head>
+<body>
+<h2>Your crush was a girl...... just after changing his gender</h2>
+</body>
+</html>
