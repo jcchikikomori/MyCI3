f2a64a5a053ac33e000e31ced1b93e4f3a59eea0
jccultima123 <johncyrillcorsanes@gmail.com>
2015-12-10 21:13:43 +0800

    Some patch for Windows using XAMPP

--- application/config/demo.php
+++ application/config/demo.php
@@ -1,7 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['APP_HOST'] = 'http://' . $_SERVER['HTTP_HOST'] .'';
+$config['APP_SUB_FOLDER'] = dirname($_SERVER['SCRIPT_NAME']);
+$config['APP_HOST'] = 'http://' . $_SERVER['HTTP_HOST'] . $config['APP_SUB_FOLDER'];
+//$config['APP_HOST'] = 'http://' . $_SERVER['HTTP_HOST'] .'';
 $config['URL'] = $config['APP_HOST'];
 
 //loading assets
