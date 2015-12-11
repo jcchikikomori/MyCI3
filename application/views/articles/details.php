b5ee9477db50c4fe7c55d5aa56fc4f7060324628
jccultima123 <johncyrillcorsanes@gmail.com>
2015-12-11 17:31:32 +0800

    Decorating some UI

--- application/views/articles/details.php
+++ application/views/articles/details.php
@@ -1,10 +1,19 @@
 <div class="container">
+	<ol class="breadcrumb">
+	  <li><a href="<?php echo $this->config->item('URL'); ?>">Home</a></li>
+	  <li><a href="<?php echo $this->config->item('URL'); ?>/articles">Articles</a></li>
+	  <li class="active"><?php echo $details['title']; ?></li>
+	</ol>
 	<div class="panel panel-default">
 		<div class="panel-heading">
-			<b>Article: <?php echo $details['title']; ?></b>
+			<h4><?php echo $details['title']; ?></h4>
 		</div>
 		<div class="panel-body">
 			<p><?php echo $details['article_body']; ?></p>
+		</div>
+		<div class="panel-footer">
+			<h4>Comments</h4>
+			None
 		</div>
 	</div>
 </div>
