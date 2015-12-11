cef829e46edb387f2ebd6a3e007f301d6d1592bf
tunimadulid <tuni.madulid@gmail.com>
2015-12-11 11:29:12 +0800

    try this cy-sama

---
+++ application/controllers/Commentajax.php
@@ -0,0 +1,48 @@
+<?php
+// if($_POST)
+// {
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Commentajax extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function cmnt()
+	{
+	$this->load->view('comment');
+
+	if($_POST)
+	{
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$comment=$_POST['comment'];
+	$lowercase = strtolower($email);
+	$image = md5( $lowercase );
+	mysql_query("insert into comments (name, email, comment) values ($name, $email, $comment) ");
+	}
+	else {
+		alert("ERROR");
+	}
+	?>
+
+	<li class="box">
+	<img src="http://www.gravatar.com/avatar.php?gravatar_id=
+	<?php echo $image; ?>"/>
+	<?php echo $name;?><br />
+	<?php echo $comment; ?>
+	</li>
+}}
