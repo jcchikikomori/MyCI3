3225ab7b585d312520926a19ca91adb60cc7700d
jccultima123 <johncyrillcorsanes@gmail.com>
2015-12-11 17:52:36 +0800

    [Error] Some annotations

--- application/controllers/Commentajax.php
+++ application/controllers/Commentajax.php
@@ -1,51 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head></head>
-<body>
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Commentajax extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * *@*see http://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function comnt()
 	{
-	$this->load->view('comment');
+		if($_POST)
+		{
+			//may something dito.... sundan mo ilan kong codes sa ibang controller
+			$name=$_POST['name'];
+			$email=$_POST['email'];
+			$comment=$_POST['comment'];
+			$lowercase = strtolower($email);
+			$image = md5( $lowercase );
+			// dapat nasa model to
+			// mysql_query("INSERT INTO comments (name,email,comment) VALUES ('$name','$email','$comment') ") ;
+		}
+		else {
+			alert("ERROR");
+		}
 
-	if($_POST)
-	{
-	$name=$_POST['name'];
-	$email=$_POST['email'];
-	$comment=$_POST['comment'];
-	$lowercase = strtolower($email);
-	$image = md5( $lowercase );
-	mysql_query("INSERT INTO comments (name,email,comment) VALUES ('$name','$email','$comment') ") ;
+		$this->load->view('comment');
 	}
-	else {
-		alert("ERROR");
-	}}}
-
-	?>
- 	
-	<li class="box">
-	<img src="http://www.gravatar.com/avatar.php?gravatar_id=
-	<?php echo $image; ?>"/>
-	<?php echo $name; ?> <br />
-	<?php echo $comment; ?>
-	</li>
-</body>
-</html>
+	 	
+	 	// dapat nasa loob ng view ito
+		<li class="box">
+		<img src="http://www.gravatar.com/avatar.php?gravatar_id=
+		<?php echo $image; ?>"/>
+		<?php echo $name; ?> <br />
+		<?php echo $comment; ?>
+		</li>
+}
