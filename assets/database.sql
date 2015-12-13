e19553a004112af90e10de5d35d0b9daa244b894
jccultima123 <johncyrillcorsanes@gmail.com>
2015-12-13 11:38:33 +0800

    Updating Database

--- assets/database.sql
+++ assets/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Dec 10, 2015 at 04:43 PM
--- Server version: 5.5.41-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.9
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2015 at 04:38 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,11 +27,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `blog_articles` (
-  `article_id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
-  `article_body` text NOT NULL,
-  PRIMARY KEY (`article_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `article_body` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `blog_articles`
+--
+
+INSERT INTO `blog_articles` (`article_id`, `title`, `article_body`) VALUES
+  (1, 'Hello World', 'Hello Justin Bieber, this is "My World". What? "What do you mean"?? Oh i am "Sorry"!');
 
 -- --------------------------------------------------------
 
@@ -40,15 +46,21 @@ CREATE TABLE IF NOT EXISTS `blog_articles` (
 --
 
 CREATE TABLE IF NOT EXISTS `blog_comments` (
-  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `emailid` varchar(100) NOT NULL,
-  `comment` text NOT NULL,
-  `time` varchar(100) NOT NULL,
+  `comment_id` int(11) NOT NULL,
+  `name` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `comment` text CHARACTER SET latin1 NOT NULL,
+  `time` varchar(100) CHARACTER SET latin1 NOT NULL,
   `cmt_article_id` int(11) NOT NULL,
-  PRIMARY KEY (`comment_id`),
-  KEY `cmt_article_id` (`cmt_article_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `user_id` bigint(10) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `blog_comments`
+--
+
+INSERT INTO `blog_comments` (`comment_id`, `name`, `email`, `comment`, `time`, `cmt_article_id`, `user_id`) VALUES
+  (1, 'jccultima123', 'johncyrillcorsanes@gmail.com', 'Looks Great!', '', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -57,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `blog_comments` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` bigint(10) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index',
+  `user_id` bigint(10) NOT NULL COMMENT 'auto incrementing user_id of each user, unique index',
   `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s name, unique',
   `user_password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'user''s password in salted and hashed format',
   `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s email, unique',
@@ -66,9 +78,60 @@ CREATE TABLE IF NOT EXISTS `users` (
   `last_name` varchar(160) COLLATE utf8_unicode_ci NOT NULL,
   `contact_no` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `user_active` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'user''s activation status',
-  `user_account_type` int(11) DEFAULT NULL COMMENT 'obsolete column',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data' AUTO_INCREMENT=1 ;
+  `user_account_type` int(11) DEFAULT NULL COMMENT 'obsolete column'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blog_articles`
+--
+ALTER TABLE `blog_articles`
+ADD PRIMARY KEY (`article_id`);
+
+--
+-- Indexes for table `blog_comments`
+--
+ALTER TABLE `blog_comments`
+ADD PRIMARY KEY (`comment_id`), ADD KEY `cmt_article_id` (`cmt_article_id`), ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `blog_articles`
+--
+ALTER TABLE `blog_articles`
+MODIFY `article_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `blog_comments`
+--
+ALTER TABLE `blog_comments`
+MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `user_id` bigint(10) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index';
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `blog_comments`
+--
+ALTER TABLE `blog_comments`
+ADD CONSTRAINT `Comment_Article_ID` FOREIGN KEY (`cmt_article_id`) REFERENCES `blog_articles` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `Comment_User` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
